9adaa1f1aed6568bb7ba94752e166d1e2a8e3d33
Emil Popovic <emil.popovic@fer.unizg.hr>
2025-10-23 21:28:51 +0200

    Dodao template za services
    dodani AuthService, EmailService, GeminiService i S3Service u zadane datoteke

---
+++ backend/src/services/__init__.py
@@ -0,0 +1,11 @@
+from .auth_service import AuthService
+from .email_service import EmailService
+from .gemini_service import GeminiService
+from .s3_service import S3Service
+
+__all__ = [
+    'AuthService',
+    'EmailService',
+    'GeminiService',
+    'S3Service'
+]
