71e4451f4cc009e47b4faf7bb40d65f3c2fa776a
Kristijan Bilanović <kristijan.bilanovic@fer.unizg.hr>
2025-10-26 17:47:05 +0100

    Added DSU color preset.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,11 +3,64 @@ import { createApp } from 'vue';
 import './style.css'
 import PrimeVue from 'primevue/config';
 import Lara from '@primeuix/themes/lara';
+import { definePreset } from '@primeuix/themes';
+
+const DailySpeakUpPreset = definePreset(Lara, {
+    semantic: {
+        primary: {
+            50: '{sky.50}',
+            100: '{sky.100}',
+            200: '{sky.200}',
+            300: '{sky.300}',
+            400: '{sky.400}',
+            500: '{sky.500}',
+            600: '{sky.600}',
+            700: '{sky.700}',
+            800: '{sky.800}',
+            900: '{sky.900}',
+            950: '{sky.950}'
+        },
+        colorScheme: {
+            light: {
+                surface: {
+                    0: '#b52323ff',
+                    50: '{zinc.50}',
+                    100: '{zinc.100}',
+                    200: '{zinc.200}',
+                    300: '{zinc.300}',
+                    400: '{zinc.400}',
+                    500: '{zinc.500}',
+                    600: '{zinc.600}',
+                    700: '{zinc.700}',
+                    800: '{zinc.800}',
+                    900: '{zinc.900}',
+                    950: '{zinc.950}'
+                }
+            },
+            dark: {
+                surface: {
+                    0: '#ffffff',
+                    50: '{slate.50}',
+                    100: '{slate.100}',
+                    200: '{slate.200}',
+                    300: '{slate.300}',
+                    400: '{slate.400}',
+                    500: '{slate.500}',
+                    600: '{slate.600}',
+                    700: '{slate.700}',
+                    800: '{slate.800}',
+                    900: '{slate.900}',
+                    950: '{slate.950}'
+                }
+            }
+        }
+    }
+});
 
 const app = createApp(App);
 app.use(PrimeVue, {
     theme: {
-        preset: Lara,
+        preset: DailySpeakUpPreset,
         options: {
             darkModeSelector: false
         }
