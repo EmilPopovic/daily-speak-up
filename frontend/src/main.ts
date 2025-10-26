345a20c028b2dc3431f8e33234b59f4b34a6b7a4
Kristijan Bilanović <kristijan.bilanovic@fer.unizg.hr>
2025-10-26 17:34:28 +0100

    Added PrimeVue plugin.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
+import App from './App.vue';
+import { createApp } from 'vue';
 import './style.css'
-import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Lara from '@primeuix/themes/lara';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Lara,
+        options: {
+            darkModeSelector: false
+        }
+    }
+});
+
+app.mount('#app');
