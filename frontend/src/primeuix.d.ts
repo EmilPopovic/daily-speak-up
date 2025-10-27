859470295c4e460aadd516192fc58c3b02ae51f4
Emil Popović <emil.popovic@fer.unizg.hr>
2025-10-27 18:10:16 +0100

    Priprema za deployment
    Napisan compose.yaml i .env.dokploy.template
    Docker compose ispravno pokrece aplikaciju
    Popravljen problem gdje TS neuspjesno builda frontend u fazi izgradnje frontend containera

---
+++ frontend/src/primeuix.d.ts
@@ -0,0 +1,8 @@
+declare module '@primeuix/themes' {
+  export function definePreset(base: any, options: any): any;
+}
+
+declare module '@primeuix/themes/lara' {
+  const Lara: any;
+  export default Lara;
+}
