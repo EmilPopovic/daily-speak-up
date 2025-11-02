17eec1fc709eb03d83003253fc1d101dfeafd9b9
mate7589 <matejakovljev@gmail.com>
2025-11-02 17:52:10 +0100

    dodan settings.py i __init__ za GeminiService klasu

--- backend/src/services/gemini_service.py
+++ backend/src/services/gemini_service.py
@@ -1,2 +1,14 @@
+from google import genai
+from google.genai.errors import APIError
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class GeminiService:
-    ...
+    def __init__(self):
+        self.api_key = os.getenv("GEMINI_API_KEY")
+        genai.configure(api_key = self.api_key)
+
+        self.model = genai.GenerativeModel("gemini-2.5-flash")
+        
