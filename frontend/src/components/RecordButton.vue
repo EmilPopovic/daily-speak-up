cf4e929d1f08545919d99d77ca88e46b82865f10
KristijanBilanovic <kristijan.bilanovic@fer.unizg.hr>
2025-11-10 23:09:40 +0100

    Added RecordButton component

---
+++ frontend/src/components/RecordButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+
+    <div class="w-[18rem] h-[18rem] aspect-square rounded-full flex justify-center
+                items-center bg-radial-[at_50%_65%] from-sky-400 to-white hover:cursor-pointer
+                mx-auto my-20 hover:scale-105 transition duration-300 shadow-lg
+                hover:bg-radial-[at_50%_65%] hover:from-sky-500 hover:to-sky-100 ">
+        <span class="pi pi-microphone"
+              style="color:white; font-size: 14rem;"></span>
+    </div>
+    
+</template>
+
+<style scoped>
+
+</style>
