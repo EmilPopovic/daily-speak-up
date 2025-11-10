39a22ae9a84d543351864a90ccc74503ac14f4eb
KristijanBilanovic <kristijan.bilanovic@fer.unizg.hr>
2025-11-10 10:12:06 +0100

    Added PrimeVue button style to Login/Logout button components

--- frontend/src/components/Logout.vue
+++ frontend/src/components/Logout.vue
@@ -1,20 +1,15 @@
 <template>
   <div>
-    <button @click="logout">Log out</button>
+    <Button @click="handleLogout" label="Log out" />
   </div>
 </template>
-<script>
-  import { useAuth0 } from '@auth0/auth0-vue';
+<script setup>
+import { useAuth0 } from '@auth0/auth0-vue';
+import Button from 'primevue/button'; 
 
-  export default {
-    setup() {
-      const { logout } = useAuth0();
+const { logout } = useAuth0();
 
-      return {
-        logout: () => {
-          logout({ logoutParams: { returnTo: window.location.origin } });
-        }
-      };
-    }
-  };
+const handleLogout = () => {
+  logout({ logoutParams: { returnTo: window.location.origin } });
+};
 </script>
