30348025d97824d0678847bcafc924d64e034144
KristijanBilanovic <kristijan.bilanovic@fer.unizg.hr>
2025-11-10 22:56:49 +0100

    Added NavBar Component

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -3,12 +3,19 @@
 
 <template>
 
-    <div class="flex flex-row w-full justify-center items-center bg-gray-500">
-        <img src="../assets/DSU_logo_circle.svg" alt="Daily Speak Up Logo" class="h-12 m-4"/>
+    <div class="flex flex-row justify-between items-center w-[95%] border border-gray-100
+                mx-2 mt-8 rounded-full p-1 shadow-lg bg-blue-50">
 
-        <div class="border-2 border-solid aspect-square rounded-[50%] border-white w-10 h-10 flex justify-center items-center mr-4 hover:scale-105 cursor-pointer transition duration-200">
-            <span class="pi pi-user" style="color:white; font-size: 1.2rem;"></span>
+        <div class="flex flex-row items-center ml-5">
+            <img src="../assets/DSU_logo_circle.svg" alt="Daily Speak Up Logo" class="h-12 m-4"/>
+            <div class="text-sky-500 font-semibold text-3xl">DailySpeakUp</div>
         </div>
+
+        <div class="flex flex-col justify-center items-center border-2 border-solid aspect-square
+                    rounded-full border-blue w-10 h-10 mr-10 hover:scale-105 cursor-pointer transition duration-200">
+            <span class="pi pi-user" style="color:blue; font-size: 1.2rem;"></span>
+        </div>
+
     </div>
     
 </template>
