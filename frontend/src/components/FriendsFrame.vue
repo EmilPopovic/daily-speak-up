12ce6ba7a9ea4a432b2d557088fde357d3077b3a
KristijanBilanovic <kristijan.bilanovic@fer.unizg.hr>
2025-11-11 23:13:44 +0100

    Added shading to the backgorunds of FriendsFrame

--- frontend/src/components/FriendsFrame.vue
+++ frontend/src/components/FriendsFrame.vue
@@ -4,12 +4,13 @@
 </script>
 
 <template>
-  <div class="flex flex-col w-[35%] h-full bg-slate-100">
-    <div class="w-full flex flex-row justify-center items-center py-2 bg-slate-200 text-slate-600 font-semibold text-3xl py-4">
+  <div class="flex flex-col w-[35%] h-full bg-linear-to-t from-slate-100 via-slate-50 to-slate-100 border-l-2
+              border-slate-300 ">
+    <div class="w-full flex flex-row justify-center items-center py-2 bg-linear-180 from-slate-200 to-slate-300 text-slate-600 font-semibold text-3xl py-4 ">
       <span class="pi pi-users mr-3" style="font-size: 2rem;"></span>
       Prijatelji
     </div>
-    <ScrollPanel style="width: 99%; height: 94.5vh">
+    <ScrollPanel style="width: 99%; height: 93%">
       
       <div class="grid grid-cols-3 gap-3 mt-8  mr-5 ml-5">
         <div v-for="i in 30" :key="i" class="m-1">
