aa957f8228caed6af6c7582f6a2287f8d657c3b9
Lara Desnica <lara.desnica@fer.unizg.hr>
2025-11-11 14:10:47 +0100

    Dodala osnovnu strukturu za ikone za prijatelje

--- frontend/src/components/FriendsFrame.vue
+++ frontend/src/components/FriendsFrame.vue
@@ -1,9 +1,55 @@
 <script setup lang="ts">
-
+  import ScrollPanel from 'primevue/scrollpanel';
 </script>
 
 <template>
   <div class="flex flex-row w-full h-full ">
+        <ScrollPanel style="width: 100%; height: 100%">
+          <div class="grid grid-cols-4 gap-4">
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            <div class="w-[100%] h-[95%] aspect-square rounded-full bg-blue-500">01 </div>
+            
+            
+
+
+            
+
+          </div>
+        </ScrollPanel>
 
   </div>
 </template>
