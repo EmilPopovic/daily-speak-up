073799e07fccc7b4b0b0a69b9247b7d391846f57
KristijanBilanovic <kristijan.bilanovic@fer.unizg.hr>
2025-11-12 20:35:52 +0100

    Added POST /userdata/interests endpoint
    Co-Authored-By: Lara Brečić <lara.brecic@fer.unizg.hr>

--- backend/src/api/v1/userdata.py
+++ backend/src/api/v1/userdata.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, FastAPI, File, UploadFile, HTTPException, Depends, status, Security
 from ..deps import get_auth_service
-from ...models import User
+from ...models import User, Interest, UserInterest
 from ...schemas import UsernameData, EmailData, InterestData, JWTPayload
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
@@ -78,3 +78,87 @@ async def set_email(
          'email': email_data.email
       }
    )
+
+@router.post("/interests", response_class=JSONResponse)
+async def set_interests(
+   interest_data: InterestData,
+   db: Session = Depends(get_db),
+   auth_result: JWTPayload = Security(get_auth_service().verify)
+):
+   auth0_user_id = auth_result['sub']
+
+   user: User | None = db.query(User).filter(
+      User.auth0_user_id == auth0_user_id
+   ).first()
+
+   if user is None:
+      raise HTTPException(
+         status_code=status.HTTP_404_NOT_FOUND, 
+         detail="User not found"
+      ) 
+   
+   unadded_interests : list[str] = []
+   added_interests : list[UserInterest] = []
+   
+   for interest in interest_data.interests:
+
+      interest_category: Interest | None = db.query(Interest).filter(
+         Interest.name.ilike(interest.strip().lower())
+      ).first()
+
+      if interest_category is None:
+         unadded_interests.append(interest)
+         continue
+
+      user_interest_exists: UserInterest | None = db.query(UserInterest).filter(
+         UserInterest.user_id == user.id,
+         UserInterest.interest_id == interest_category.id
+      ).first()
+
+      if user_interest_exists is not None:
+         unadded_interests.append(interest)
+         continue
+
+      new_user_interest = UserInterest(
+         user_id=user.id,
+         interest_id=interest_category.id
+      )
+
+      added_interests.append(new_user_interest)
+
+   try:
+      db.add_all(added_interests)
+      db.commit()
+   except Exception as e:
+      db.rollback()
+      raise HTTPException(
+         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+         detail=f"Error adding interests: {str(e)}"
+      )
+   
+   if len(unadded_interests) == 0:
+      return JSONResponse(
+         status_code=status.HTTP_200_OK,
+         content={
+            "message": "All interests updated successfully", 
+            'interests': interest_data.interests
+         }
+      )
+   elif len(unadded_interests) == len(interest_data.interests):
+      raise HTTPException(
+         status_code=status.HTTP_400_BAD_REQUEST, 
+         detail="No interests were found to add or they don't match existing categories in DB"
+      )
+
+   else:
+      added_interests_str = [
+         interest for interest in interest_data.interests 
+         if interest not in unadded_interests
+      ]
+      return JSONResponse(
+         content={
+            "message": "Some interests were not found and/or could not be added", 
+            'added_interests': added_interests_str,
+            'unadded_interests': unadded_interests
+         }
+      )
