e20f81eaddd7729ac4f71ad0d0d093cb79caa0b7
KristijanBilanovic <kristijan.bilanovic@fer.unizg.hr>
2025-11-14 14:25:07 +0100

    Protected POST topic endpoint
    Co-Authored-By: Mate Jakovljev <mate.jakovljev@fer.unizg.hr>

--- backend/src/api/v1/topic.py
+++ backend/src/api/v1/topic.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from requests import session
 
 from ...schemas import TopicRequest, TopicResponse
 from ...services.gemini_service import GeminiService
-from ..deps import get_gemini_service
-
+from ..deps import get_gemini_service, get_session
+from supertokens_python.recipe.session import SessionContainer 
 
 router = APIRouter(prefix='/topics', tags=['Topics'])
 
@@ -15,7 +16,8 @@ router = APIRouter(prefix='/topics', tags=['Topics'])
 )
 async def generate_topic(
     request: TopicRequest,
-    gemini_service: GeminiService = Depends(get_gemini_service)
+    gemini_service: GeminiService = Depends(get_gemini_service),
+    session: SessionContainer = Depends(get_session)
 ):
     try:
         tema = await gemini_service.generate_topic(request.interes, request.lang)
